9bb7e5fbf064aa9f8afe7d9401347d1a23e57bc2
Christian Elsen <chriselsen@users.noreply.github.com>
2016-10-10 15:54:32 -0700

    Added support for CloudWatch
    Data is also pushed into CloudWatch.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+
 exports.handler = (event, context, callback) => {
     var dns = require('dns');
     var wait = require('wait.for');
     var output = "";
 
+	var xApiDomain = event.headers["x-api-domain"];
+	if (!xApiDomain) { xApiDomain = 'unknown' }
+	console.log("x-api-domain: ", xApiDomain);
+	
     var ipArray = event.headers["X-Forwarded-For"].split(",");
     console.log("X-Forwarded-For: ", event.headers["X-Forwarded-For"]);
-    output += "ipver=" + (ipArray[0].indexOf(":") > -1 ? 'IPv6' : 'IPv4');
+	ipVersion = (ipArray[0].indexOf(":") > -1 ? 'IPv6' : 'IPv4');
+    output += "ipver=" + ipVersion;
     output += require('os').EOL;
 
     var viaArray = event.headers["Via"].split(" ");
-    output += "httpver=" + viaArray[0].replace('https/','');
+	httpVersion = viaArray[0].replace('https/','')
+    output += "httpver=" + httpVersion;
     output += require('os').EOL;
 
     function DNSLookup(){
@@ -22,9 +29,62 @@ exports.handler = (event, context, callback) => {
 		edgeLoc = (loc[0].split("."))[1].substring(0, 3);
         output += "edgeloc=" + edgeLoc;
 		console.log("EdgeLoc: ", edgeLoc);
+		
+		putCloudWatch(xApiDomain, ipVersion, httpVersion, edgeLoc, callback);
+		
         callback(null, output);
     }
 
     wait.launchFiber(DNSLookup);
     
 };
+
+function putCloudWatch(Domain, IPVersion, HTTPVersion, EdgeLoc, callback){
+    var AWS = require('aws-sdk');
+    AWS.config.region = 'us-west-2';
+    var cloudwatch = new AWS.CloudWatch();
+    
+    var params = {
+            MetricData: [
+                {
+                    MetricName: 'IP-Version', /* required */
+                    Dimensions: [
+                        {
+                            Name: Domain, /* required */
+                            Value: IPVersion /* required */
+                        },
+                    ],
+                    Timestamp: + Math.floor(new Date() / 1000),
+                    Unit: 'Count',
+                    Value: 1.0 
+                },
+                {
+                    MetricName: 'HTTP-Version', /* required */
+                    Dimensions: [
+                        {
+                            Name: Domain, /* required */
+                            Value: HTTPVersion /* required */
+                        },
+                    ],
+                    Timestamp: + Math.floor(new Date() / 1000),
+                    Unit: 'Count',
+                    Value: 1.0
+                },
+				{
+                    MetricName: 'Edge Location', /* required */
+                    Dimensions: [
+                        {
+                            Name: Domain, /* required */
+                            Value: EdgeLoc /* required */
+                        },
+                    ],
+                    Timestamp: + Math.floor(new Date() / 1000),
+                    Unit: 'Count',
+                    Value: 1.0 
+                }
+            ],
+            Namespace: 'CloudFrontStats' /* required */
+        };
+        
+        cloudwatch.putMetricData(params, function(err, data) {});
+}
